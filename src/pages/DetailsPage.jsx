5cf3c4271497e096d73a26141dfdbfed1ea66dbc
mohammadsajjadjamali <mohammadsajjadjamali@gmail.com>
2025-03-07 15:44:19 +0330

    edit DetailsPage File

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -5,42 +5,39 @@ import { SiOpenproject } from "react-icons/si";
 import { IoMdPricetag } from "react-icons/io";
 import { FaArrowLeft } from "react-icons/fa";
 import styles from "./DetailsPage.module.css";
-import { useEffect } from "react";
 
 const DetailsPage = () => {
   const { id } = useParams();
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
   const productDetails = useProductsDetails(+id);
   if (!productDetails) return <Loader />;
   return (
-    <div className={styles.container}>
-      <img src={productDetails.image} alt={productDetails.title} />
-      <div className={styles.information}>
-        <h3 className={styles.title}>{productDetails.title}</h3>
-        <p className={styles.description}>{productDetails.description}</p>
-        <p className={styles.category}>
-          <SiOpenproject />
-          {productDetails.category}
-        </p>
-        <div>
-          <span className={styles.price}>
-            {" "}
-            <IoMdPricetag />
-            {productDetails.price}$
-          </span>
-          <Link to="/products">
-            {" "}
-            <span>
-              <FaArrowLeft />
-              Back to Shop
-            </span>{" "}
-          </Link>
+    <>
+      <div className={styles.container}>
+        <img src={productDetails.image} alt={productDetails.title} />
+        <div className={styles.information}>
+          <h3 className={styles.title}>{productDetails.title}</h3>
+          <p className={styles.description}>{productDetails.description}</p>
+          <p className={styles.category}>
+            <SiOpenproject />
+            {productDetails.category}
+          </p>
+          <div>
+            <span className={styles.price}>
+              {" "}
+              <IoMdPricetag />
+              {productDetails.price}$
+            </span>
+            <Link to="/products">
+              {" "}
+              <span>
+                <FaArrowLeft />
+                Back to Shop
+              </span>{" "}
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
